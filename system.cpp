9c17ffb440087d1b5d20c28e334795ebb03e05db
Surya Handika Putratama <ubunteroz@gmail.com>
2015-02-22 22:59:45 +0700

    system.cpp: Add SHA3 algorithm. Use UTF8 instead of Latin1.

--- system.cpp
+++ system.cpp
@@ -16,7 +16,7 @@ QString ignsystem::cliOut(const QString& command){
 QString ignsystem::hash(const QString &data, QString hash_func){
     bool isValid = true;
     QByteArray hash;
-    QByteArray byteArray = data.toLatin1();
+    QByteArray byteArray = data.toUtf8();
 
     if (hash_func == "md4"){
         hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Md4);
@@ -32,6 +32,14 @@ QString ignsystem::hash(const QString &data, QString hash_func){
         hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha384);
     } else if (hash_func == "sha512"){
         hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha512);
+    } else if (hash_func == "sha3-224"){
+        hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha3_224);
+    } else if (hash_func == "sha3-256"){
+        hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha3_256);
+    } else if (hash_func == "sha3-384"){
+        hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha3_384);
+    } else if (hash_func == "sha3-512"){
+        hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha3_512);
     } else {
         isValid = false;
     }
